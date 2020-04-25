9f3f2ab712ecbbf1a6acd9b98d00aff6b83d7a58
Hammad Khan <hammadkhn30@gmail.com>
2020-04-25 14:43:28 +0530

    Intl package added in the pubspec and made changes in the the date format of the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -50,7 +51,46 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orangeAccent,
+                          width: 2,
+                        )
+                      ),
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        '\$${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize:20,
+                          color: Colors.orangeAccent,
+                        )
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title, style: TextStyle(
+                          fontSize: 16,
+                          fontWeight:FontWeight.bold,
+                        )),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          )
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           )
