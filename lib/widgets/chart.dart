439f79851e174627f29b87cd891c8889b017f405
Hammad Khan <hammadkhn30@gmail.com>
2020-05-02 21:51:55 +0530

    added chart in the app

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -12,7 +13,7 @@ class Chart extends StatelessWidget {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
-      double totalSum;
+      var totalSum=0.0;
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
@@ -27,6 +28,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -34,7 +41,8 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return
+          return ChartBar(data['day'], data['amount'],
+              (data['amount'] as double) / totalSpending);
         }).toList(),
       ),
     );
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -38,7 +38,7 @@ class ChartBar extends StatelessWidget {
               ),
             ],
           ),
-        )
+        ),
       ],
     );
   }
