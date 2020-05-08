c1da0eda0725ecf0a1edec17d2ffb086cb359565
Hammad Khan <hammadkhn30@gmail.com>
2020-05-08 23:30:43 +0530

    added the database for data storage

---
+++ lib/widgets/dbhelper.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import '../model/model.dart';
+class DbHelper {
+  static String tblDocs = "docs";
+  String docId = "id";
+  String docTitle = "title";
+  double docAmount = "amount";
+  DateTime docDate = "date";
+
+  static final DbHelper _dbHelper = DbHelper._internal();
+  DbHelper._internal();
+  factory DbHelper() {
+    return _dbHelper;
+  }
+
+  static Database _db;
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDb();
+    }
+    return _db;
+  }
+
+  Future<Database> initializeDb() async {
+    Directory d = await getApplicationDocumentsDirectory();
+    String p = d.path + "/docexpire.db";
+    var db = await openDatabase(p, version: 1, onCreate: _createDb);
+    return db;
+  }
+
+  void _createDb(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE $tblDocs($docId INTEGER PRIMARY KEY, $docTitle TEXT,
+            + $docAmount INTEGER, )"
+    );
+  }
+
+  Future<int> insertDoc(Doc doc) async {
+    var r;
+    Database db = await this.db;
+    try {
+      r = await db.insert(tblDocs, doc.toMap());
+    }
+    catch (e) {
+      debugPrint("insertDoc: " + e.toString());
+    }
+    return r;
+  }
